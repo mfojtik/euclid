3171a4435b1164d605d69fa6a17f13108b4742b3
Michal Fojtik <mfojtik@redhat.com>
2024-03-12 11:48:41 +0100

    update build

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 mkdir -p ~/go/bin
 
-go build -o scrape ./scraper && mv scrape ~/go/bin/euclid-scraper
-go build -o render ./renderer && mv render ~/go/bin/euclid-render
+GOFLAGS="-mod=vendor" go build -o scrape ./scraper && mv scrape ~/go/bin/euclid-scraper
+GOFLAGS="-mod=vendor" go build -o render ./renderer && mv render ~/go/bin/euclid-render
